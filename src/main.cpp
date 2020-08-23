4d3e33231e3755c3f01ad21d13e2907b2bed3301
guiweiwei <554079678@qq.com>
2020-08-23 13:51:48 +0800

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+#include "templateFactory.h"
+
+class F
+{
+public:
+	F(int i) {}
+};
+
+void test(DEFAULT_FACTORY* factory)
+{
+	auto z = factory->createHeader(2.0);
+}
+
+int main()
+{
+	templateFactory<F, pool_mode>* factory = new templateFactory<F, pool_mode>;
+
+	F *x = factory->createFactory(1);
+
+	DEFAULT_FACTORY dFactory;
+
+	auto y = dFactory.createItem(1);
+
+	test(&dFactory);
+
+	auto k = dFactory.createValue();
+	auto k1 = dFactory.createValue(1);
+	auto k2 = dFactory.createValue(2, 3);
+
+	auto x2 = OBJECT_NEW(F, 1);
+}
---
+++ src/templateFactory.h
@@ -0,0 +1,90 @@
+#pragma once
+#include <iostream>
+
+template <typename T, typename ... Args>
+T* __new(const Args&...args)
+{
+	return new T(args...);
+}
+
+#define OBJECT_NEW(T, ...) __new<T>(__VA_ARGS__)
+
+struct pool_mode {};
+struct normal_mode {};
+
+#define DEFAULT_MODE normal_mode
+#define DEFAULT_FACTORY Factory<DEFAULT_MODE>
+
+typedef int SignalItemData;
+typedef double SignalHeadInfo;
+class SignalValueInfo
+{
+public:
+	SignalValueInfo() {}
+	SignalValueInfo(int) {}
+	SignalValueInfo(double, int) {}
+};
+
+template <typename T>
+class Pool
+{
+public:
+	void* allocate() { return malloc(sizeof(T)); }
+};
+
+template <class T, class P = normal_mode>
+class templateFactory
+{
+public:
+	template <typename ... Args>
+	T* createFactory(const Args&...args)
+	{
+		std::cout << "normal mode create" << std::endl;
+		return new T(args...);
+	}
+};
+
+
+template <class T>
+class templateFactory<T, pool_mode>
+{
+public:
+	template <typename ... Args>
+	T* createFactory(const Args&...args)
+	{
+		std::cout << "pool mode create" << std::endl;
+		void* addr = m_pool.allocate();
+		return new(addr) T(args...);
+	}
+
+private:
+	Pool<T> m_pool;
+};
+
+template <typename M>
+class Factory
+{
+public:
+	template <typename ... Args>
+	SignalItemData* createItem(const Args&... args)
+	{
+		return m_itemFactory.createFactory(args...);
+	}
+
+	template <typename ... Args>
+	SignalHeadInfo* createHeader(const Args& ... args)
+	{
+		return m_headerFactory.createFactory(args...);
+	}
+
+	template <typename ... Args>
+	SignalValueInfo* createValue(const Args& ... args)
+	{
+		return m_valueFactory.createFactory(args...);
+	}
+
+private:
+	templateFactory<SignalItemData, M> m_itemFactory;
+	templateFactory<SignalHeadInfo, M> m_headerFactory;
+	templateFactory<SignalValueInfo, M> m_valueFactory;
+};
